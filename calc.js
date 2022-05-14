fd50678427a26c1ff2800a40e2f42fd76b721f39
Teemu <teemu9598@gmail.com>
2022-05-14 18:42:07 +0300

    Add event listeners for buttons

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+//Math functions
 function add(num1, num2) {
     return num1 + num2
 } 
@@ -13,3 +14,43 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 } 
+
+//Program functions
+function operate(operation, num1, num2) {
+    return
+}
+
+function clearScreen() {
+    solved = []
+    current = []
+}
+
+function addNumberToCurrent(number){
+    current.push(number)
+}
+
+
+//Main program
+OPERATORS = ['add', 'subtract', 'multiply', 'divide', 'equals']
+operating = false
+solved = []
+current = []
+
+buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.id === 'clear') {
+            clearScreen()
+            console.log('clear screen')
+        }
+
+        else if (!isNaN(button.id)) {
+            
+        }
+
+        else if (OPERATORS.includes(button.id)) {
+            console.log('operator')
+        }
+    })
+})
