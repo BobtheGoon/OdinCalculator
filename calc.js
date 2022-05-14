864a179323547daa0072ed34f63538d1148f13c2
Teemu <teemu9598@gmail.com>
2022-05-14 23:04:59 +0300

    Change solved and current from arrays to strings

--- calc.js
+++ calc.js
@@ -17,7 +17,27 @@ function divide(num1, num2) {
 
 //Program functions
 function operate(operation, num1, num2) {
-    return
+    if (operation === 'add') {
+        solution = add(num1, num2)
+    }
+
+    else if (operation === 'subtract') {
+        solution = subtract(num1, num2)
+    }
+
+    else if (operation === 'multiply') {
+        solution = multiply(num1, num2)
+    }
+
+    else if (operation === 'divide') {
+        solution = divide(num1, num2)
+    }
+
+    else if (operation === 'equals') {
+        return
+    }
+
+    return solution
 }
 
 function clearScreen() {
@@ -26,15 +46,26 @@ function clearScreen() {
 }
 
 function addNumberToCurrent(number){
-    current.push(number)
+    current += number
+}
+
+function startsWithZero(){
+    if (current.length > 0) {
+        return false
+    }
+    return true
+}
+
+function updateCurrentDisplay(){
+    return
 }
 
 
 //Main program
 OPERATORS = ['add', 'subtract', 'multiply', 'divide', 'equals']
 operating = false
-solved = []
-current = []
+solved = ''
+current = ''
 
 buttons = document.querySelectorAll('button')
 
@@ -46,7 +77,13 @@ buttons.forEach((button) => {
         }
 
         else if (!isNaN(button.id)) {
-            
+            if (button.id === '0' && startsWithZero()) {
+                console.log('cannot start with zero')
+            }
+            else {
+                addNumberToCurrent(button.id)
+                console.log(current)
+            }
         }
 
         else if (OPERATORS.includes(button.id)) {
